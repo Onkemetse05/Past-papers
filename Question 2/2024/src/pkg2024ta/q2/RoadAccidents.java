9efa67ab47db9fa91f1f6f852596d7e266afc6c0
Onkemetse05 <oregolelemangwegape05@gmail.com>
2025-09-29 00:03:07 +0200

    Update RoadAccidents.java
    added constructor

--- Question 2/2024/src/pkg2024ta/q2/RoadAccidents.java
+++ Question 2/2024/src/pkg2024ta/q2/RoadAccidents.java
@@ -1,8 +1,30 @@
 package pkg2024ta.q2;
 
 public abstract class RoadAccidents implements IRoadAccidents{
-    String vehicleType;
-    String city;
-    int accidentTotal;
+    private String vehicleType;
+    private String city;
+    private int accidentTotal;
+    
+    public RoadAccidents(String vehicleType, String city, int accidentTotal){
+        this.vehicleType = vehicleType;
+        this.city = city;
+        this.accidentTotal = accidentTotal;
+    }
+    
+    @Override
+    public String getAccidentVehicleType(){
+        return vehicleType;
+    }
+    
+    @Override
+    public String getCity(){
+        return city;
+    }
+    
+    @Override
+    public int getAccidentTotal(){
+        return accidentTotal;
+    }
+    
 
 }
