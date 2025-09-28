3609ce20709d2890c26460f5c7d5efb76a37c0eb
Onkemetse05 <oregolelemangwegape05@gmail.com>
2025-09-29 00:02:24 +0200

    Update RoadAccidentReport.java
    corrected syntax

--- Question 2/2024/src/pkg2024ta/q2/RoadAccidentReport.java
+++ Question 2/2024/src/pkg2024ta/q2/RoadAccidentReport.java
@@ -1,23 +1,19 @@
 package pkg2024ta.q2;
 
 public class RoadAccidentReport extends RoadAccidents {
-
-    @Override
-    public String getVehicleType() {   
-        return vehicleType;
-    
+    public RoadAccidentReport(String vehicleType, String city, int accidentTotal){
+        super(vehicleType, city, accidentTotal);
     }
-
-    @Override
-    public String getCity() {    
-        return city;
     
-    }
-
-    @Override
-    public int getAccidentTotal() {
-        return accidentTotal;
-  
+    public void printAccidentReport(){
+        System.out.println();
+        System.out.println("VEHICLE ACCIDENT REPORT");
+        System.out.println("************************");
+        System.out.println("VEHICLE TYPE: " + getAccidentVehicleType());
+        System.out.println("CITY: " + getCity());
+        System.out.println("ACCIDENT TOTAL: " + getAccidentTotal());
+        System.out.println("************************");
+                
     }
      
 }
